07c14171c3f01ea9775db0740ec647a2cf1cef29
Matthew Baya <chiptiep99@gmail.com>
2024-03-20 20:33:19 +0800

    update folder data json, dan migration addcolumn

--- migrations/20240320105953-AddFKProduct.js
+++ migrations/20240320105953-AddFKProduct.js
@@ -3,7 +3,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Products', 'CategoryId', Sequelize.INTEGER, {
+    await queryInterface.addColumn('Products', 'CategoryId', {
+      type: Sequelize.INTEGER, 
       references:{
         model: "Categories",
         key: 'id'
