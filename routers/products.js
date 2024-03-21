86ea66306f16075afc5750851988c2b89e166eb1
Matthew Baya <chiptiep99@gmail.com>
2024-03-21 13:06:33 +0800

    tambah ejs, fitur add & edit product

--- routers/products.js
+++ routers/products.js
@@ -4,10 +4,11 @@ const ProductCtrl = require("../ctrls/product");
 const router = express.Router();
 
 router.get("/", ProductCtrl.showProducts);
-router.get("/add");
-router.post("/add");
-router.get("/edit/:productId");
-router.post("/edit/:productId");
+router.get("/add", ProductCtrl.addProducts);
+router.post("/add", ProductCtrl.saveProduct);
+router.get("/edit/:productId", ProductCtrl.editProducts);
+router.post("/edit/:productId", ProductCtrl.postEditProducts);
 router.get("/:productId/order/:userId");
 router.post("/:productId/order/:userId");
+router.post("/:productId/delete", ProductCtrl.deleteProduct);
 module.exports = router;
