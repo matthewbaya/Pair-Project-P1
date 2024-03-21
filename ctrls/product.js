a31c61b3898393eaafb811f56ea5b4fa9bab36c1
Matthew Baya <chiptiep99@gmail.com>
2024-03-21 16:36:16 +0800

    update

--- ctrls/product.js
+++ ctrls/product.js
@@ -1,13 +1,36 @@
 "use strict";
+const { Op } = require('sequelize');
+
 const { Product, User, Category } = require("../models");
+const product = require("../models/product");
+const { query } = require('express');
 
 class ProductCtrl {
   //* ─── Main Page ───────────────────────────────────────────────────────
   static async showProducts(req, res) {
     try {
-      let products = await Product.findAll({ include: Category });
+      let {searchProduct, searchCategory} = req.query;
+      let options = {
+        include: {model: Category,
+        where: {}
+      },
+        order: [['CategoryId', 'ASC']],
+        where: {}
+      }
+      if (searchProduct){
+        options.where.name =  {
+          [Op.iLike]: `%${searchProduct}%`
+      }
+      }
+      if (searchCategory){
+        options.include.where.name =  {
+          [Op.iLike]: `%${searchCategory}%`
+      }
+      }
+
+      let products = await Product.findAll(options);
+      // res.send(products)
       res.render('landing-admin', {products});
-      // res.send(products);
     } catch (error) {
       console.log(error);
       res.send(error.message);
@@ -19,7 +42,6 @@ class ProductCtrl {
     try {
       let products = await Product.findAll();
       let category = await Category.findAll()
-      // res.send(category)
       res.render("add-product", {products, category});
     } catch (error) {
       console.log(error);
@@ -31,23 +53,21 @@ class ProductCtrl {
   static async saveProduct(req, res) {
     try {
       let {name, price, description, CategoryId} = req.body
-      // res.send({name, price, description, CategoryId})
       await Product.create({name, price, description, CategoryId});
       res.redirect('/')
     } catch (error) {
       console.log(error);
-      res.send(error.message);
+      res.send(error);
     }
   }
 //* ─── Delete Product ────────────────────────────────────────────────────
 static async deleteProduct(req,res){
   try {
-    // let { productId } = req.params;
-    // let data = await Product.findByPk(productId)
-    res.send(req.params)
-    // await Employee.destroy({
-    //     where: { id: employeeId }
-    // });
+    let { productId } = req.params;
+    await Product.destroy({
+        where: { id: productId }
+    });
+    res.redirect('/')
   } catch (error) {
     res.send(error)
   }
@@ -81,6 +101,44 @@ static async deleteProduct(req,res){
       res.send(error)
     }
   }
+
+static async customerPage(req, res){
+  try {
+    let {searchProduct, searchCategory} = req.query;
+    let options = {
+      include: {model: Category,
+      where: {}
+    },
+      order: [['CategoryId', 'ASC']],
+      where: {}
+    }
+    if (searchProduct){
+      options.where.name =  {
+        [Op.iLike]: `%${searchProduct}%`
+    }
+    }
+    if (searchCategory){
+      options.include.where.name =  {
+        [Op.iLike]: `%${searchCategory}%`
+    }
+    }
+
+    let products = await Product.findAll(options);
+    // res.send(products)
+    res.render('landing-customer', {products});
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+}
+
+static async detailProducts(req,res){
+  try {
+    res.send(masuk)
+  } catch (error) {
+    res.send(error)
+  }
+}
 
   Order;
   static async order(req, res) {
--- routers/products.js
+++ routers/products.js
@@ -4,11 +4,13 @@ const ProductCtrl = require("../ctrls/product");
 const router = express.Router();
 
 router.get("/", ProductCtrl.showProducts);
+router.get("/customer", ProductCtrl.customerPage);
 router.get("/add", ProductCtrl.addProducts);
 router.post("/add", ProductCtrl.saveProduct);
+router.get("/detail/:productId", ProductCtrl.detailProducts);
 router.get("/edit/:productId", ProductCtrl.editProducts);
 router.post("/edit/:productId", ProductCtrl.postEditProducts);
 router.get("/:productId/order/:userId");
 router.post("/:productId/order/:userId");
-router.post("/:productId/delete", ProductCtrl.deleteProduct);
+router.get("/:productId/delete", ProductCtrl.deleteProduct);
 module.exports = router;
