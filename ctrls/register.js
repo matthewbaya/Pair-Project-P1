5f7017c6db7b9a60cc8e722f259f2437b9c6702e
Matthew Baya <chiptiep99@gmail.com>
2024-03-21 17:00:40 +0800

    upd

--- ctrls/register.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class Register {}
-
-module.exports = Register;
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const UserController = require('../ctrls/userController')
 
 router.use("/register", require("./register"));
 router.use("/login", require("./login"));
